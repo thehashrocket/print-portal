0d172f10292a076f2ebd0a8f2d351d942c56a966
Jason Shultz <jasshultz@gmail.com>
2024-07-31 13:04:59 -0700

    updated eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
-  },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
-  ],
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
