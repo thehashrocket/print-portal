cfd10d0a19e2576aff02adf9576a584bca449b1f
Jason Shultz <jasshultz@gmail.com>
2024-08-20 19:48:38 -0700

    trying to remove prisma accelerate

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,7 +6,7 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  })
+  });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
