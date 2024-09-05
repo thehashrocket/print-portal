552a5b67cb7dd08dbbf2415d1ff5ab1c4fa83372
Jason Shultz <jasshultz@gmail.com>
2024-09-05 12:38:37 -0500

    fixed job link on dashboard

--- src/app/_components/dashboard/draggableOrderItemsDash.tsx
+++ src/app/_components/dashboard/draggableOrderItemsDash.tsx
@@ -93,7 +93,7 @@ const DraggableOrderItemsDash: React.FC<{ initialOrderItems: SerializedOrderItem
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 m-2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                                 </svg>
-                                <a href={`/order/${orderItem.orderId}/orderItem/${orderItem.id}`} className="text-blue-400 hover:underline">View Job</a>
+                                <a href={`/orders/${orderItem.orderId}/orderItem/${orderItem.id}`} className="text-blue-400 hover:underline">View Job</a>
                             </div>
                         </div>
                     ))}
