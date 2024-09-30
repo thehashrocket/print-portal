f8f309d3d06b575c69c5de29af04b6efe3d27981
Jason Shultz <jasshultz@gmail.com>
2024-09-30 08:21:25 -0700

    invoice syncing is working

--- src/server/api/routers/quickbooks/qbInvoice.ts
+++ src/server/api/routers/quickbooks/qbInvoice.ts
@@ -1,14 +1,28 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { refreshTokenIfNeeded } from "~/services/quickbooksService";
 import { TRPCError } from "@trpc/server";
+import { XMLParser } from 'fast-xml-parser';
 import axios from 'axios';
 import { z } from 'zod';
-async function fetchInvoices(ctx: any, accessToken: string, companyID: string) {
-    const response = await axios.get(`https://quickbooks.api.intuit.com/v3/company/${companyID}/invoice`, {
+async function fetchInvoices(ctx: any, accessToken: string, companyID: string, customerID: string) {
+    let query = `SELECT * from Invoice`;
+
+    const baseUrl = process.env.QUICKBOOKS_ENVIRONMENT === 'sandbox'
+        ? 'https://sandbox-quickbooks.api.intuit.com'
+        : 'https://quickbooks.api.intuit.com';
+
+    const parser = new XMLParser();
+
+    const queryUrl = `${baseUrl}/v3/company/${companyID}/query?query=${encodeURIComponent(query)}`;
+
+    const response = await axios.get(queryUrl, {
         headers: {
-            'Authorization': `Bearer ${accessToken}`
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/xml',
         }
     });
+    console.log(response.data);
+
     return response.data;
 }
 
@@ -21,14 +35,21 @@ export const qbInvoiceRouter = createTRPCRouter({
                 select: { quickbooksRealmId: true },
             });
 
-            if (!user?.quickbooksRealmId) {
-                throw new TRPCError({
-                    code: 'UNAUTHORIZED',
-                    message: 'Not authenticated with QuickBooks',
-                });
-            }
+            // Look through the offices and find the all that has a quickbooksCustomerId
+            const offices = await ctx.db.office.findMany({
+                where: {
+                    quickbooksCustomerId: { not: null }
+                }
+            });
 
-            const invoices = await fetchInvoices(ctx, accessToken, user.quickbooksRealmId);
+            // Look through the offices and get the invoices for each office
+            const invoices = await Promise.all(offices.map(async (office) => {
+                if (office.quickbooksCustomerId && user?.quickbooksRealmId) {
+                    return fetchInvoices(ctx, accessToken, user.quickbooksRealmId, office.quickbooksCustomerId);
+                }
+            }));
+
+
             return invoices;
         }),
     // Get Invoice by Company ID
